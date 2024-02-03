29e6c0aedd4b84a302d01ef4131beee02ad29759
Julia Alberto <julia.elyssa.alberto@gmail.com>
2024-02-03 15:49:46 -0800

    front and back set up

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import IndexMSG from "./components/indexMSG";
+
+export default function Home(){
+    return (
+        <div>
+            <h1> HELLO </h1>
+            <IndexMSG />
+        </div>
+    )
+}
